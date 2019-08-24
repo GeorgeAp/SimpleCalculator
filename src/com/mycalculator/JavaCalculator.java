dfcf09b9060cdf145b7aa2ae0a33e51f5100cde0
georges <george.apaaboah@gmail.com>
2019-08-24 13:43:36 +0200

    Updated files commit

--- src/com/mycalculator/JavaCalculator.java
+++ src/com/mycalculator/JavaCalculator.java
@@ -5,6 +5,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class JavaCalculator {
+    private double total1 = 0.0; // set variable to store values
+    private double total2 = 0.0;
+    private char math_operator;
+
     private JPanel JavaCalculator;
     private JTextField txtDisplay;
     private JButton btnClear;
@@ -24,6 +28,13 @@ public class JavaCalculator {
     private JButton btnAdd;
     private JButton btnDivide;
     private JButton btnEqual;
+    private JLabel labelDeveloperName;
+
+    private void getOperator(String btnText){
+        math_operator = btnText.charAt(0);
+        total1 = total1 + Double.parseDouble(txtDisplay.getText());
+        txtDisplay.setText("");
+    }
 
     public JavaCalculator() {
         btn1.addActionListener(new ActionListener() {
@@ -113,13 +124,93 @@ public class JavaCalculator {
 
             }
         });
+
+        // actions ie plus, minus, multiply, divide ...
+        btnAdd.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String buttonText = btnAdd.getText();
+                getOperator(buttonText);
+            }
+        });
+
+        btnSubtract.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String buttonText = btnSubtract.getText();
+                getOperator(buttonText);
+            }
+        });
+
+        btnClear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                total2 = 0.0;
+                txtDisplay.setText("");
+            }
+        });
+
+        btnDot.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (txtDisplay.getText().equals("")){
+                    txtDisplay.setText("0.");
+                }
+                else if (txtDisplay.getText().contains(".")){
+                    btnDot.setEnabled(false);
+                }
+                else {
+                    String btnDotText = txtDisplay.getText() + btnDot.getText();
+                    txtDisplay.setText(btnDotText);
+                }
+            }
+        });
+        btnDivide.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String buttonText = btnDivide.getText();
+                getOperator(buttonText);
+            }
+        });
+        btnMultiply.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String buttonText = btnMultiply.getText();
+                getOperator(buttonText);
+            }
+        });
+
+        btnEqual.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                switch (math_operator){
+                    case '+':
+                        total2 = total1 + Double.parseDouble(txtDisplay.getText());
+                        break;
+                    case '-':
+                        total2 = total1 - Double.parseDouble(txtDisplay.getText());
+                        break;
+                    case '/':
+                        total2 = total1 / Double.parseDouble(txtDisplay.getText());
+                        break;
+                    case '*':
+                        total2 = total1 * Double.parseDouble(txtDisplay.getText());
+                        break;
+                }
+                txtDisplay.setText(Double.toString(total2));
+                total1 = 0.0;
+            }
+        });
     }
 
     public static void main(String[] args) {
+        ImageIcon img = new ImageIcon("C:/Users/Utilisateur/Documents/Java/calculator_icon.png");
         JFrame frame = new JFrame("JavaCalculator");
         frame.setContentPane(new JavaCalculator().JavaCalculator);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setIconImage(img.getImage());
         frame.pack();
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
 }
